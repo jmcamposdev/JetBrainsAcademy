667a1ab0dff91cf4bf8f0cc2eaf6d50f02893892
Jose Maria <jmcamposdev@gmail.com>
2023-03-24 10:46:02 +0100

    AddingStage 2 Rock-Papers-Scissors

--- JavaforBeginners/Hard/Rock-Paper-Scissors/src/rock/paper/scissors/RockPaperScissors.java
+++ JavaforBeginners/Hard/Rock-Paper-Scissors/src/rock/paper/scissors/RockPaperScissors.java
@@ -12,14 +12,22 @@ import java.util.Scanner;
  */
 public class RockPaperScissors {
 
+    public static Choice[][] winningCombinations = {
+            {Choice.ROCK, Choice.SCISSORS},
+            {Choice.PAPER, Choice.ROCK},
+            {Choice.SCISSORS, Choice.PAPER}
+    };
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.next();
-        switch (userInput) {
-            case "rock" -> System.out.println("Sorry, but the computer chose paper");
-            case "paper" -> System.out.println("Sorry, but the computer chose scissors");
-            case "scissors" -> System.out.println("Sorry, but the computer chose rock");
-            default -> System.out.println("Error!");
+        Choice userChoice = Choice.valueOf(userInput.toUpperCase());
+        Choice computerChoice = Choice.values()[(int) (Math.random() * Choice.values().length)];
+        if (computerChoice == userChoice) {
+            System.out.println("There is a draw (" + computerChoice + ")");
+        } else if (userChoice == winningCombinations[computerChoice.ordinal()][1]) {
+            System.out.println("Sorry, but the computer chose " + computerChoice);
+        } else {
+            System.out.println("Well done. The computer chose " + computerChoice + " and failed");
         }
     }
     
