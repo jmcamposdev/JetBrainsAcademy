a8af4f17cee3b79894d9bdfa5b7dcb91c0de0e93
Jose Maria <jmcamposdev@gmail.com>
2023-03-27 13:00:52 +0200

    Stage 4 ReadabilityScore

--- JavaforBeginners/Challenging/ReadabilityScore/src/readabilityscore/ReadabilityScore.java
+++ JavaforBeginners/Challenging/ReadabilityScore/src/readabilityscore/ReadabilityScore.java
@@ -6,7 +6,11 @@ package readabilityscore;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -15,31 +19,102 @@ import java.util.Scanner;
 public class ReadabilityScore {
 
     public class Main {
-    public static void main(String[] args) {
-        StringBuilder text = new StringBuilder();
-        try (FileReader fileReader = new FileReader(args[0])) {
-            Scanner scanner = new Scanner(fileReader);
-            while (scanner.hasNext()) {
-                text.append(scanner.nextLine());
+static int wordsNum = 0;
+    static int sentencesNum = 0;
+    static int charactersNum = 0;
+    static int syllablesNum = 0;
+    static int polysyllablesNum = 0;
+
+    public static void main(String[] args) throws IOException {
+        String text = readFromFile(args[0]);
+        String[] sentences = Pattern.compile("[?!.][\\s]*").split(text);
+        for (String sentence : sentences) {
+            String[] words = Pattern.compile("\\s").split(sentence);
+            for (String word : words) {
+                int temp = getSyllables(word);
+                if (temp > 2) {
+                    polysyllablesNum++;
+                }
+                syllablesNum += temp;
             }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            wordsNum += words.length;
         }
-        System.out.println("The text is:");
-        System.out.println(text);
+        sentencesNum = sentences.length;
+        charactersNum = text.length() - wordsNum + 1;
 
-        String[] sentences = text.toString().split("[.!?]");
-        int words = text.toString().split("\\s+").length;
-        int characters = text.toString().replaceAll("\\s+", "").length();
-        double score = 4.71 * (characters / (double) words) + 0.5 * (words / (double) sentences.length) - 21.43;
+        System.out.println("The text is: " + "\n" + text + "\n");
+        System.out.println("Words: " + wordsNum);
+        System.out.println("Sentences: " + sentencesNum);
+        System.out.println("Characters: " + charactersNum);
+        System.out.println("Syllables: " + syllablesNum);
+        System.out.println("Polysyllables: " + polysyllablesNum);
 
-        System.out.println("Words: " + words);
-        System.out.println("Sentences: " + sentences.length);
-        System.out.println("Characters: " + characters);
-        System.out.println("The score is: " + score);
-        System.out.println("This text should be understood by " + (score <= 1 ? "5-6" : score <= 2 ? "6-7" : score <= 3 ? "7-8" : score <= 4 ? "8-9" : score <= 5 ? "9-19" : score <= 6 ? "10-11" : score <= 7 ? "11:12" : score <= 8 ? "12-13" : score <= 9 ? "13-14" : score <= 10 ? "14-15" : score <= 11 ? "15-16" : score <= 12 ? "16-17" : score <= 13 ? "17-18" : "18-22") + " year olds.");
-
+        System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+        String decision = new Scanner(System.in).next();
+        System.out.println();
+        boolean all = "all".equals(decision);
+        int sum = 0;
+        if ("ARI".equals(decision) || all) {
+            double score = 4.71 * (1.0 * charactersNum / wordsNum) + 0.5 * (1.0 * wordsNum / sentencesNum) - 21.43;
+            System.out.print("Automated Readability Index: " + new DecimalFormat("#0.00").format(score));
+            System.out.println(" (about " + getAge(score) +" year olds).");
+            sum += getAge(score);
+        }
+        if ("FK".equals(decision) || all) {
+            double score = 0.39 * (1.0 * wordsNum / sentencesNum) + 11.8 * (1.0 * syllablesNum / wordsNum) - 15.59;
+            System.out.print("Flesch–Kincaid rename.readability tests: " + new DecimalFormat("#0.00").format(score));
+            System.out.println(" (about " + getAge(score) +" year olds).");
+            sum += getAge(score);
+        }
+        if ("SMOG".equals(decision) | all) {
+            double score = 1.043 * Math.sqrt(polysyllablesNum * (30.0 / sentencesNum)) + 3.1291;
+            System.out.print("Simple Measure of Gobbledygook: " + new DecimalFormat("#0.00").format(score));
+            System.out.println(" (about " + getAge(score) +" year olds).");
+            sum += getAge(score);
+        }
+        if ("CL".equals(decision) || all) {
+            double score = 0.0588 * (100.0 * charactersNum / wordsNum) - 0.296 * (100.0 * sentencesNum / wordsNum) - 15.8;
+            System.out.print("Coleman–Liau index: " + new DecimalFormat("#0.00").format(score));
+            System.out.println(" (about " + getAge(score) +" year olds).");
+            sum += getAge(score);
+        }
+        System.out.println("This text should be understood in average by " + new DecimalFormat("#0.00").format(1.0 * sum / 4.0) + " year olds.");
     }
-}
+
+    private static int getSyllables(String word) {
+        int counter = 0;
+        boolean wasVowel = false;
+        for (int i = 0; i < word.length(); i++) {
+            char c = Character.toLowerCase(word.charAt(i));
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
+                if (!wasVowel) {
+                    wasVowel = true;
+                    counter++;;
+                }
+            } else {
+                wasVowel = false;
+            }
+        }
+        if (word.charAt(word.length() - 1) == 'e') {
+            counter--;
+        }
+        if (counter == 0) {
+            counter = 1;
+        }
+        return counter;
+    }
+
+    private static String readFromFile(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    private static int getAge(double score) {
+        final int[] age = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 25};
+        int intScore = (int) Math.ceil(score);
+        if (intScore >= age.length - 1) {
+            intScore = age.length - 1;
+        }
+        return age[intScore];
+    }}
     
 }
